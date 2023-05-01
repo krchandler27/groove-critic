f3ab9b0c639ae5676828c6c0aa4768e2af63a48f
Katherine Lantto <kathlantto@gmail.com>
2023-04-30 22:17:32 -0700

    stylign changes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {BiLogOut} from 'react-icons/bi'
 import Auth from '../../utils/auth';
 //🎸🎹🥁🎻🎷🎺//
 const Header = () => {
@@ -12,19 +12,19 @@ const Header = () => {
     <header className="mb-4 py-3 flex-row align-center headerContainer">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div className='headerTitle'>
-          <Link className="text-light" to="/">
-            <h1 className="m-0" style={{fontFamily: "Mystery Quest"}}>Groove Critic</h1>
+          <Link className="" to="/">
+            <h1 className="m-0 title" style={{}}>Groove Critic</h1>
           </Link>
         </div>
-        <div>
+        <div className='nav'>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-md btn-info m-2" to="/me">
+              <Link className="btn btn-md btn-info m-2 myProfile" to="/me">
                 {Auth.getProfile().data.username}'s profile
               </Link>
-              <button className="btn btn-md btn-light m-2" onClick={logout}>
-                Logout
-              </button>
+              <a className="logout" onClick={logout}>
+                <BiLogOut size={'3em'}/>
+              </a>
             </>
           ) : (
             <>
